1759d09705e45b30cdf7fe56baea3e16984bc750
Sean Li <lishang106@gmail.com>
2013-03-23 18:37:15 -0400

    Initial push with customized user model.

---
+++ core/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from core.models import FSUser
+
+
+class FSUserAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        ('Status', {
+            'classes': ('collapse',),
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'last_login')
+        }),
+        ('Groups & Permissions', {
+            'classes': ('collapse',),
+            'fields': ('groups', 'user_permissions')
+        }),
+    )
+    list_display = ('email', 'is_staff', 'last_login')
+    search_fields = ['email']
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'last_login')
+    filter_horizontal = ['groups', 'user_permissions']
+
+
+admin.site.register(FSUser, FSUserAdmin)
