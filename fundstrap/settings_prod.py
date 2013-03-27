75abec85ce572c9c34583f1d8f4be84096d584c1
Sean Li <lishang106@gmail.com>
2013-03-27 15:14:15 -0400

    Added raven configuration for Sentry.

--- fundstrap/settings_prod.py
+++ fundstrap/settings_prod.py
@@ -18,3 +18,8 @@ COMPRESS_STORAGE = STATICFILES_STORAGE
 
 STATIC_URL = 'https://s3.amazonaws.com/fundstrap/'
 MEDIA_URL = 'https://s3.amazonaws.com/fundstrap/'
+
+# Set your DSN value
+RAVEN_CONFIG = {
+    'dsn': 'https://72751d7b7a174ea8932d0e6fb8f1e192:e17a9554929d4f92b966bbb4c0023e3c@app.getsentry.com/6931',
+}
